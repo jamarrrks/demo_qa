9c857f9ae6881934623c2cf9020611069527ce23
Valeriya_Chernykh <jamarks@mail.ru>
2024-06-10 09:41:52 +0300

    Commit for the lesson

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,19 +1,19 @@
-# from pages.demoqa import DemoQa
-# import time
-#
-#
-# def test_check_icon(browser):
-#     # browser.get('https://demoqa.com/')
-#     #
-#     # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
-#     #
-#     # if icon is None:
-#     #     print('Не найден')
-#     # else:
-#     #     print('Найден')
-#
-#     demo_qa_page = DemoQa(browser)
-#     demo_qa_page.visit()
-#     demo_qa_page.icon.click()
-#     assert demo_qa_page.equal_url()
-#     assert demo_qa_page.icon.exist()
+from pages.demoqa import DemoQa
+import time
+
+
+def test_check_icon(browser):
+    # browser.get('https://demoqa.com/')
+    #
+    # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+    #
+    # if icon is None:
+    #     print('Не найден')
+    # else:
+    #     print('Найден')
+
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.icon.click()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.icon.exist()
--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -5,7 +5,6 @@ def test_check_text1(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
 
-    # assert demo_qa_page.footer.exist()
     assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
 
@@ -14,6 +13,5 @@ def test_check_text2(browser):
     demo_qa_page.visit()
 
     demo_qa_page.btn_elements.click()
-    # assert demo_qa_page.page_text.exist()
     assert demo_qa_page.page_text.get_text() == 'Please select an item from left to start practice.'
 
