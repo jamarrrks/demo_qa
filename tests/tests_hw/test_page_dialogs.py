4003eee8512c74c7e83b7b5c98ab1350d7e1ca4f
Valeriya_Chernykh <jamarks@mail.ru>
2024-06-22 14:51:57 +0300

    Commit for the lesson

--- tests/tests_hw/test_page_dialogs.py
+++ tests/tests_hw/test_page_dialogs.py
@@ -1,11 +1,11 @@
 from pages.modal_dialogs import ModalDialogs
 
 
-# def test_modal_elements(browser):
-#     mod_el_pg = ModalDialogs(browser)
-#     mod_el_pg.visit()
-#
-#     assert mod_el_pg.btns.check_count_elements(count=5)
+def test_modal_elements(browser):
+    mod_el_pg = ModalDialogs(browser)
+    mod_el_pg.visit()
+
+    assert mod_el_pg.btns.check_count_elements(count=5)
 
 
 def test_navigation_modal(browser):
